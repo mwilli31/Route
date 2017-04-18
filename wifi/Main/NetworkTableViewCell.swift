8d39d296bb4d5cfd89f4feaf3ff48e3439335238
sajayshah <shahsajay@gmail.com>
2017-04-17 21:03:39 -0700

    cell for requests view + code cleanup

--- wifi/Main/NetworkTableViewCell.swift
+++ wifi/Main/NetworkTableViewCell.swift
@@ -16,44 +16,6 @@ class NetworkTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        
-//        let gap : CGFloat = 20
-//        let labelHeight: CGFloat = 30
-//        let titleWidth: CGFloat = self.bounds.width - (2*gap)
-//        let labelWidth: CGFloat = 100
-//        let lineGap : CGFloat = 5
-//        let label2Y : CGFloat = gap + labelHeight + lineGap
-//        let imageSize : CGFloat = 30
-//        
-//        contentView.backgroundColor = .clear
-//        
-//        let whiteRoundedView : UIView = UIView(frame: CGRect(x: 10, y: 8, width: self.bounds.width, height: 100))
-//        
-//        whiteRoundedView.layer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 1.0, 0.9])
-//        whiteRoundedView.layer.masksToBounds = false
-//        whiteRoundedView.layer.cornerRadius = 2.0
-//        whiteRoundedView.layer.shadowOffset = CGSize(width: -1, height: 1)
-//        whiteRoundedView.layer.shadowOpacity = 0.2
-//        
-//        contentView.addSubview(whiteRoundedView)
-//        contentView.sendSubview(toBack: whiteRoundedView)
-//        
-//        wifiNameLabel = UILabel()
-//        wifiNameLabel.frame = CGRect(x:gap, y:gap, width:titleWidth, height:labelHeight)
-//        wifiNameLabel.textColor = .black
-//        wifiNameLabel.font = wifiNameLabel.font.withSize(24)
-//        contentView.addSubview(wifiNameLabel)
-//        
-//        allowedLabel = UILabel()
-//        allowedLabel.frame = CGRect(x: imageSize + (2*gap), y:label2Y, width:labelWidth, height:labelHeight)
-//        allowedLabel.textColor = .black
-//        contentView.addSubview(allowedLabel)
-//        
-//        activeLabel = UILabel()
-//        activeLabel.frame = CGRect(x: imageSize + (5*gap) + allowedLabel.bounds.width, y:label2Y, width:labelWidth, height:labelHeight)
-//        activeLabel.textColor = .black
-//        contentView.addSubview(activeLabel)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- wifi/Main/RequestsViewController.swift
+++ wifi/Main/RequestsViewController.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
+
+
+class RequestTableViewCell: UITableViewCell {
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var userPicture: UIImageView!
+    
+}
+
 class RequestsViewController: UIViewController {
+    @IBOutlet weak var requestsTableView: UITableView!
     
     class func instantiateFromStoryboard() -> RequestsViewController {
         let storyboard = UIStoryboard(name: "Requests", bundle: nil)
@@ -18,6 +27,8 @@ class RequestsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.requestsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,4 +36,44 @@ class RequestsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+}
+
+extension RequestsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    }
+    
+    // Swipe to show options not working because of PagingMenuViewController?
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let button1 = UITableViewRowAction.init(style: .default, title: "Action 1") { (action, IndexPath) in
+            print("HELLO")
+        }
+        let button2 = UITableViewRowAction.init(style: .default, title: "Action 2") { (action, IndexPath) in
+            print("BYE")
+        }
+        
+        return [button1, button2]
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Instantiate a cell
+        let cellIdentifier = "request"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! RequestTableViewCell
+        
+        // Adding the right information
+        cell.username.text = "Sajay"
+
+        // Returning the cell
+        return cell
+    }
 }
